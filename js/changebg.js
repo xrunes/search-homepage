7b32c8f6611dbc2314bd52619c426151bdd694af
xrunes <xrunes@outlook.com>
2022-04-08 12:31:13 +0800

    相对路径bug

--- js/changebg.js
+++ js/changebg.js
@@ -3,7 +3,7 @@ var b = document.getElementById('rotation');
 var r = document.getElementById('rod');
 b.onclick = function(){
     if(a.style.backgroundImage == 'none'){
-        a.style.backgroundImage = "url(../img/1295632_2736-x-1824-wallpaper.jpg)";
+        a.style.backgroundImage = "url(./img/1295632_2736-x-1824-wallpaper.jpg)";
         r.style.backgroundColor = 'rgb(254, 254, 254)';
     }
     else{
