87fbed029ff599adedb1c80fa4ca4f8c172f984e
xrunes <xrunes@outlook.com>
2023-06-05 20:11:41 +0800

    将更改背景图改为暗黑模式

--- js/changebg.js
+++ js/changebg.js
@@ -1,13 +1,21 @@
 var a = document.getElementById('bg');
 var b = document.getElementById('rotation');
 var r = document.getElementById('rod');
-b.onclick = function(){
+/*b.onclick = function() {
     if(a.style.backgroundImage == 'none'){
-        a.style.backgroundImage = "url(./img/1295632_2736-x-1824-wallpaper.jpg)";
+        a.style.backgroundImage = "url(./img/98124138_p0.jpg)";
         r.style.backgroundColor = 'rgb(254, 254, 254)';
     }
     else{
         a.style.backgroundImage = 'none';
         r.style.backgroundColor = 'wheat'
+    }
+}*/
+b.onclick = function() {
+    if(a.style.backgroundColor == 'white') {
+        a.style.backgroundColor = 'rgba(50,50,50,1)';
+    }
+    else{
+        a.style.backgroundColor = 'white';
     }
 }
